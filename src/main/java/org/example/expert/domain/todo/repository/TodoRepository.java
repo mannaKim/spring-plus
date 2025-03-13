9e8db0c43f968f4f1208f1571b5078332e887c2f
hanna <hannah.k0925@gmail.com>
2025-03-13 19:30:11 +0900

    8. QueryDSL
    - findAllByOrderByModifiedAtDesc 메서드도 QueryDSL 방식으로 변환

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,34 +1,8 @@
 package org.example.expert.domain.todo.repository;
 
 import org.example.expert.domain.todo.entity.Todo;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryCustom {
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-
-    @Query("""
-        SELECT t FROM Todo t 
-        LEFT JOIN FETCH t.user u 
-        WHERE (:weather IS NULL OR t.weather = :weather)
-            AND (:startDateTime IS NULL OR t.modifiedAt >= :startDateTime)
-            AND (:endDateTime IS NULL OR t.modifiedAt <= :endDateTime)
-        ORDER BY t.modifiedAt DESC
-    """)
-    Page<Todo> findAllByOrderByModifiedAtDesc(
-            Pageable pageable,
-            @Param("weather") String weather,
-            @Param("startDateTime") LocalDateTime startDateTime,
-            @Param("endDateTime") LocalDateTime endDateTime
-    );
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
