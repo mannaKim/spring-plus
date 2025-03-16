104d40c53aa53ae847e098d14fff32346b46d53b
hanna <hannah.k0925@gmail.com>
2025-03-17 00:58:48 +0900

    10. QueryDSL 을 사용하여 검색 기능 만들기
    - 페이징 반환 방식 PageImpl 대신 PageableExecutionUtils를 사용하도록 변경
    - 매니저 저장 시 이미 등록된 담당자인 경우 에러 반환하도록 수정

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -7,6 +7,7 @@ import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
+import org.example.expert.domain.todo.dto.response.TodoSearchResponse;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
 import org.example.expert.domain.user.dto.response.UserResponse;
@@ -86,5 +87,10 @@ public class TodoService {
                 todo.getCreatedAt(),
                 todo.getModifiedAt()
         );
+    }
+
+    public Page<TodoSearchResponse> searchTodos(int page, int size, String keyword, String nickname, LocalDateTime startDate, LocalDateTime endDate) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        return todoRepository.searchTodos(pageable, keyword, startDate, endDate, nickname);
     }
 }
